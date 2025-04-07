68f5cf29b964e1c5e96a7e0068f6229e88ac96de
Anna Sas <anna.sas.dev@gmail.com>
2025-04-07 13:03:32 +0200

    Feat: Replace custom HTML with MarkdownEditor component
    Replaced the manual HTML structure for the markdown editor with the reusable `<MarkdownEditor>` component. This simplifies the code and leverages the component's built-in functionality, improving maintainability and consistency.

---
+++ src/InfiniLore.Blazor/Services/JsRuntimeHelper.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace InfiniLore.Blazor.Services;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[InjectableScoped<IJsRuntimeHelper>]
+public class JsRuntimeHelper(IJSRuntime jsRuntime) : IJsRuntimeHelper {
+    public async Task<int> GetSelectionStartAsync(ElementReference element) {
+        return await jsRuntime.InvokeAsync<int>("getSelectionStart", element);
+    }
+    public async Task<int> GetSelectionEndAsync(ElementReference element) {
+        return await jsRuntime.InvokeAsync<int>("getSelectionEnd", element);
+    }
+}
--- src/InfiniLore.Blazor/wwwroot/InfiniLoreBlazor.js
+++ src/InfiniLore.Blazor/wwwroot/InfiniLoreBlazor.js
@@ -1,2 +1,19 @@
-﻿window.getSelectionStart = (element) => element.selectionStart;
-window.getSelectionEnd = (element) => element.selectionEnd;
+﻿window.getSelectionStart = (element) => _getSelection(
+    element,
+    (element) => element.selectionStart
+)
+
+window.getSelectionEnd = (element) => _getSelection(
+    element,
+    (element) => element.selectionEnd
+);
+
+const _getSelection = (element, callback) => {
+    if (!(element && (element.tagName.toLowerCase() === "input" || element.tagName.toLowerCase() === "textarea"))) {
+        console.error("Element must be an input or textarea.");
+        return -1;
+    } 
+    return callback(element);
+}
+
+
