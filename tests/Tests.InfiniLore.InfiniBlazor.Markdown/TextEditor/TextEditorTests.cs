2a75cf423335693b6f7c012d7462f60e2c98a11d
Anna Sas <anna.sas.dev@gmail.com>
2025-04-18 22:57:14 +0200

    Change: Rename test methods for clarity

--- tests/Tests.InfiniLore.InfiniBlazor.Markdown/TextEditor/TextEditorTests.cs
+++ tests/Tests.InfiniLore.InfiniBlazor.Markdown/TextEditor/TextEditorTests.cs
@@ -17,7 +17,7 @@ public class TextEditorTests(ITextEditor textEditor) {
     [Test]
     [MethodDataSource(typeof(ModifyBoldMultiLineDataSources), nameof(ModifyBoldMultiLineDataSources.HelloWoldDataSource))]
     [MethodDataSource(typeof(ModifyItalicMultiLineDataSources), nameof(ModifyItalicMultiLineDataSources.HelloWoldDataSource))]
-    public async Task Modify_HelloWorld_ByRange(string sectionName, int start, int end, string expected) {
+    public async Task Modify_SpecificRange(string sectionName, int start, int end, string expected) {
         // Arrange
         var source = new TextSource { Text = "Hello World!" };
         var expectedSource = new TextSource { Text = expected };
@@ -33,7 +33,7 @@ public class TextEditorTests(ITextEditor textEditor) {
     [Test]
     [MethodDataSource(typeof(ModifyBoldMultiLineDataSources),nameof(ModifyBoldMultiLineDataSources.DataSources))]
     [MethodDataSource(typeof(ModifyItalicMultiLineDataSources),nameof(ModifyItalicMultiLineDataSources.DataSources))]
-    public async Task Modify_MultiLine(string sectionName, string input, string expected) {
+    public async Task Modify_WholeRange(string sectionName, string input, string expected) {
         // Arrange
         var source = new TextSource { Text =input };
         var expectedSource = new TextSource { Text = expected };
