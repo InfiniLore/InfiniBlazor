2015b978b795b70406e25a12fda61c76a07f679c
Anna Sas <anna.sas.dev@gmail.com>
2025-04-20 20:40:11 +0200

    Feat: Add dynamic table generation for Markdown tests

--- tests/Tests.InfiniLore.InfiniBlazor.Markdown/MarkdownParser/DataSources/TableDataSources.cs
+++ tests/Tests.InfiniLore.InfiniBlazor.Markdown/MarkdownParser/DataSources/TableDataSources.cs
@@ -1,6 +1,9 @@
 ﻿// ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.Markdown;
+using InfiniLore.InfiniBlazor.Markdown.MdNodes;
+
 namespace Tests.InfiniLore.InfiniBlazor.Markdown.MarkdownParser.DataSources;
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
@@ -35,7 +38,21 @@ public class TableDataSources {
                     </tr>
                 </tbody>
             </table>
-            """
+            """,
+            static rootNode => {
+                IMdNode table = rootNode.AddTable();
+                IMdNode tableHeader = table.AddTableHead();
+                IMdNode tableHeaderRow = tableHeader.AddTableRow();
+                tableHeaderRow.AddTableHeadCell().WithContent("Column 1");
+                tableHeaderRow.AddTableHeadCell().WithContent("Column 2");
+                tableHeaderRow.AddTableHeadCell().WithContent("Column 3");
+                IMdNode tableBody = table.AddTableBody();
+                IMdNode tableBodyRow = tableBody.AddTableRow();
+                tableBodyRow.AddTableCell().WithContent("Row 2 col 1");
+                tableBodyRow.AddTableCell().WithContent("Row 2 col 2");
+                tableBodyRow.AddTableCell().WithContent("Row 2 col 3");
+            }
+            
         );
 
         yield return static () => new MarkdownTestDto(SectionName,
@@ -106,7 +123,48 @@ public class TableDataSources {
                     </tr>
                 </tbody>
             </table>
-            """
+            """,
+            static rootNode => {
+                IMdNode table = rootNode.AddTable();
+                IMdNode tableHeader = table.AddTableHead();
+                IMdNode tableHeaderRow = tableHeader.AddTableRow();
+                tableHeaderRow.AddTableHeadCell().WithContent("Tables");
+                tableHeaderRow.AddTableHeadCell().WithContent("Are");
+                tableHeaderRow.AddTableHeadCell().WithContent("Cool");
+                IMdNode tableBody = table.AddTableBody();
+                IMdNode tableRow1 = tableBody.AddTableRow();
+                tableRow1.AddTableCell().WithContent("col 3 is");
+                tableRow1.AddTableCell().WithContent("right-aligned");
+                tableRow1.AddTableCell().WithContent("$1600");
+                IMdNode tableRow2 = tableBody.AddTableRow();
+                tableRow2.AddTableCell().WithContent("col 2 is");
+                tableRow2.AddTableCell().WithContent("centered");
+                tableRow2.AddTableCell().WithContent("$12");
+                IMdNode tableBodyRow3 = tableBody.AddTableRow();
+                tableBodyRow3.AddTableCell().WithContent("zebra stripes");
+                tableBodyRow3.AddTableCell().WithContent("are neat");
+                tableBodyRow3.AddTableCell().WithContent("$1");
+                
+                IMdNode table2 = rootNode.AddTable();
+                IMdNode tableHeader2 = table2.AddTableHead();
+                IMdNode tableHeaderRow2 = tableHeader2.AddTableRow();
+                tableHeaderRow2.AddTableHeadCell().WithContent("Tables");
+                tableHeaderRow2.AddTableHeadCell().WithContent("Are");
+                tableHeaderRow2.AddTableHeadCell().WithContent("Cool");
+                IMdNode tableBody2 = table2.AddTableBody();
+                IMdNode tableRow12 = tableBody2.AddTableRow();
+                tableRow12.AddTableCell().WithContent("col 3 is");
+                tableRow12.AddTableCell().WithContent("right-aligned");
+                tableRow12.AddTableCell().WithContent("$1600");
+                IMdNode tableRow22 = tableBody2.AddTableRow();
+                tableRow22.AddTableCell().WithContent("col 2 is");
+                tableRow22.AddTableCell().WithContent("centered");
+                tableRow22.AddTableCell().WithContent("$12");
+                IMdNode tableBodyRow32 = tableBody2.AddTableRow();
+                tableBodyRow32.AddTableCell().WithContent("zebra stripes");
+                tableBodyRow32.AddTableCell().WithContent("are neat");
+                tableBodyRow32.AddTableCell().WithContent("$1");
+            }
         );
     }
 }
