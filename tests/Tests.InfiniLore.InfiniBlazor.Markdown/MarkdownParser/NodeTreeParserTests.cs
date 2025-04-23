c962db078b21432e5b65bfbeb1dcaf097a81f8af
Anna Sas <anna.sas.dev@gmail.com>
2025-04-23 18:05:24 +0200

    Feat: Split MarkdownParserTests into separate NodeTreeParserTests

---
+++ tests/Tests.InfiniLore.InfiniBlazor.Markdown/MarkdownParser/NodeTreeParserTests.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.Markdown;
+using InfiniLore.InfiniBlazor.Markdown.MdNodes;
+using Tests.InfiniLore.InfiniBlazor.Markdown.MarkdownParser.DataSources;
+
+namespace Tests.InfiniLore.InfiniBlazor.Markdown.MarkdownParser;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[DIDataSource]
+public class NodeTreeParserTests(IMdNodeTreeParser nodeTreeParser)  {
+
+    [Test]
+    [MethodDataSource(typeof(AggregateDataSources), nameof(AggregateDataSources.DataSources))]
+    [MethodDataSource(typeof(BlockQuoteDataSources), nameof(BlockQuoteDataSources.DataSources))]
+    [MethodDataSource(typeof(BoldAndItalicDataSources), nameof(BoldAndItalicDataSources.DataSources))]
+    [MethodDataSource(typeof(BoldDataSources), nameof(BoldDataSources.DataSources))]
+    [MethodDataSource(typeof(CodeDataSources), nameof(CodeDataSources.DataSources))]
+    [MethodDataSource(typeof(CodeInlineDataSources), nameof(CodeInlineDataSources.DataSources))]
+    [MethodDataSource(typeof(EdgeCaseDataSources), nameof(EdgeCaseDataSources.DataSources))]
+    [MethodDataSource(typeof(EmoteDataSources), nameof(EmoteDataSources.DataSources))]
+    [MethodDataSource(typeof(EscapedCharacterDataSources), nameof(EscapedCharacterDataSources.DataSources))]
+    [MethodDataSource(typeof(HeadingDataSources), nameof(HeadingDataSources.DataSources))]
+    [MethodDataSource(typeof(HorizontalLineDataSources), nameof(HorizontalLineDataSources.DataSources))]
+    [MethodDataSource(typeof(HtmlDataSources), nameof(HtmlDataSources.DataSources))]
+    [MethodDataSource(typeof(ItalicDataSources), nameof(ItalicDataSources.DataSources))]
+    [MethodDataSource(typeof(LinkDataSources), nameof(LinkDataSources.DataSources))]
+    [MethodDataSource(typeof(ListsDataSources), nameof(ListsDataSources.DataSources))]
+    [MethodDataSource(typeof(SpecialCharacterDataSources), nameof(SpecialCharacterDataSources.DataSources))]
+    [MethodDataSource(typeof(StrikethroughDataSources), nameof(StrikethroughDataSources.DataSources))]
+    [MethodDataSource(typeof(SubAndSuperScriptDataSources), nameof(SubAndSuperScriptDataSources.DataSources))]
+    [MethodDataSource(typeof(SubScriptDataSources), nameof(SubScriptDataSources.DataSources))]
+    [MethodDataSource(typeof(SuperScriptDataSources), nameof(SuperScriptDataSources.DataSources))]
+    [MethodDataSource(typeof(TableDataSources), nameof(TableDataSources.DataSources))]
+    [MethodDataSource(typeof(TagDataSources), nameof(TagDataSources.DataSources))]
+    [MethodDataSource(typeof(UnderlineDataSources), nameof(UnderlineDataSources.DataSources))]
+    [MethodDataSource(typeof(XSSDataSources), nameof(XSSDataSources.DataSources))]
+    public async Task ParseToNodeTree_ValidInputs(MarkdownTestDto dto) {
+        // Arrange
+        var nodeTree = new MdNodeTree();
+
+        // Act
+        nodeTreeParser.ParseToNodeTree(dto.Markdown, nodeTree);
+        Skip.When(dto.ExpectedNode == null, "The node tree is null and thus cannot be compared.");
+
+        // Assert
+        await Assert.That(nodeTree.RootNode).IsEquivalentTo(dto.ExpectedNode);
+    }
+    
+}
