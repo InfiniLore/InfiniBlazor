3030384ed2ee0b78fce3a36b02f5816eafb8a703
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-04-24 19:12:43 +0200

    Change: Refactor pool creation methods for improved clarity

--- src/InfiniLore.InfiniBlazor.Markdown/Services/PoolCache.cs
+++ src/InfiniLore.InfiniBlazor.Markdown/Services/PoolCache.cs
@@ -13,14 +13,14 @@ namespace InfiniLore.InfiniBlazor.Markdown;
 // ---------------------------------------------------------------------------------------------------------------------
 public static class PoolCache {
     public static readonly ObjectPool<MarkdownParserEngine> RunningMarkdownParserPool = CreateResettablePool<MarkdownParserEngine>(4);
-    public static readonly ObjectPool<Dictionary<int, MarkdownElement>> DepthCachePool = CreateCollectionPool<Dictionary<int, MarkdownElement>, KeyValuePair<int, MarkdownElement>>(2);
+    public static readonly ObjectPool<Dictionary<int, MarkdownElement>> DepthCachePool = CreateDictionaryPool<int, MarkdownElement>(2);
     public static readonly ObjectPool<MarkdownSyntaxVisitor> MarkdownSyntaxVisitorPool = CreateResettablePool<MarkdownSyntaxVisitor>(24);
     public static readonly ObjectPool<MarkdownFragment> MarkdownFragmentPool = CreateResettablePool<MarkdownFragment>(256);
-    public static readonly ObjectPool<Stack<Range>> RangeStackPool = CreateStackPool<Stack<Range>, Range>(16);
+    public static readonly ObjectPool<Stack<Range>> RangeStackPool = CreateStackPool<Range>(16);
     public static readonly ObjectPool<Stack<MarkdownSyntaxVisitor>> MMarkdownSyntaxVisitorStackPool = CreateMdNodeVisitorStackPool(4);
     public static readonly ObjectPool<StringBuilder> StringBuilderPool = CreateStringBuilderPool(8);
     public static readonly ObjectPool<MarkdownSyntaxNode> MdNodePool = CreateResettablePool<MarkdownSyntaxNode>(256);
-    public static readonly ObjectPool<Stack<MarkdownSyntaxNode>> MdNodeStackPool = CreateStackPool<Stack<MarkdownSyntaxNode>, MarkdownSyntaxNode>(16);
+    public static readonly ObjectPool<Stack<MarkdownSyntaxNode>> MdNodeStackPool = CreateStackPool<MarkdownSyntaxNode>(16);
     public static readonly ObjectPool<MarkdownSyntaxTree> MdNodeTreePool = CreateResettablePool<MarkdownSyntaxTree>(8);
     
     // -----------------------------------------------------------------------------------------------------------------
@@ -29,11 +29,11 @@ public static class PoolCache {
     private static ObjectPool<T> CreateResettablePool<T>(int maxRetained) where T : class, IResettable, new()
         => new DefaultObjectPool<T>(new ResettablePoolPolicy<T>(), maxRetained);
 
-    private static ObjectPool<T> CreateCollectionPool<T, TItem>(int maxRetained) where T : class, ICollection<TItem>, new() 
-        => new DefaultObjectPool<T>(new CollectionPoolPolicy<T, TItem>(), maxRetained);
+    private static ObjectPool<Dictionary<TKey, TValue>> CreateDictionaryPool<TKey, TValue>(int maxRetained) where TKey : notnull 
+        => new DefaultObjectPool<Dictionary<TKey, TValue>>(new CollectionPoolPolicy<Dictionary<TKey, TValue>, KeyValuePair<TKey, TValue>>(), maxRetained);
 
-    private static ObjectPool<T> CreateStackPool<T, TItem>(int maxRetained) where T : Stack<TItem>, new() 
-        => new DefaultObjectPool<T>(new StackPoolPolicy<T,TItem>(), maxRetained);
+    private static ObjectPool<Stack<TItem>> CreateStackPool<TItem>(int maxRetained)  
+        => new DefaultObjectPool<Stack<TItem>>(new StackPoolPolicy<Stack<TItem>,TItem>(), maxRetained);
     
     private static ObjectPool<Stack<MarkdownSyntaxVisitor>> CreateMdNodeVisitorStackPool(int maxRetained) 
         => new DefaultObjectPool<Stack<MarkdownSyntaxVisitor>>(new MarkdownSyntaxVisitorStackPoolPolicy(MarkdownSyntaxVisitorPool), maxRetained);
