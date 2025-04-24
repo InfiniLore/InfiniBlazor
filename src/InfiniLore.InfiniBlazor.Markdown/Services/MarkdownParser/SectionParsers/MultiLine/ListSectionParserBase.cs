6f4356b239277f74c6d41ccf16f6091500c40ad8
Anna Sas <anna.sas.dev@gmail.com>
2025-04-24 13:34:59 +0200

    Change: Refactor list parsers into a shared abstract base class

--- src/InfiniLore.InfiniBlazor.Markdown/Services/MarkdownParser/SectionParsers/MultiLine/ListSectionParserBase.cs
+++ src/InfiniLore.InfiniBlazor.Markdown/Services/MarkdownParser/SectionParsers/MultiLine/ListSectionParserBase.cs
@@ -9,33 +9,36 @@ namespace InfiniLore.InfiniBlazor.Markdown.SectionParsers.MultiLine;
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-[InjectableSingleton<ISectionHandler>("listUnordered")]
-public class ListUnorderedSectionParser : ISectionHandler {
-
+public abstract class ListSectionParserBase : ISectionHandler {
     private static readonly int LTaskId = MarkdownRegexLib.GetListGroupId("lTask");
     private static readonly int LHeadId = MarkdownRegexLib.GetListGroupId("lHead");
     private static readonly int LBodyId = MarkdownRegexLib.GetListGroupId("lBody");
+    
     public ParserOrigin SkipOnOrigin => ParserOrigin.NotSkipped;
+    
+    protected abstract MdElement ListType { get; }
+
     // -----------------------------------------------------------------------------------------------------------------
     // Methods
     // -----------------------------------------------------------------------------------------------------------------
     public void HandleMatch(IRunningMarkdownParser parser, IMdNode currentNode, Match entireMatch, Group group, ParserOrigin origin) {
-        if (!entireMatch.TryGetValue(out string? listUnorderedBody)) return;
+        if (!entireMatch.TryGetValue(out string? listBody)) return;
 
-        MatchCollection matchCollection = MarkdownRegexLib.ListItemBodyRegex.Matches(listUnorderedBody);
+        MatchCollection matchCollection = MarkdownRegexLib.ListItemBodyRegex.Matches(listBody);
         int matchCount = matchCollection.Count;
         Match[] matchArray = ArrayPool<Match>.Shared.Rent(matchCount);
+        
         try {
             matchCollection.CopyTo(matchArray, 0);
 
-            IMdNode ulNode = currentNode.AddChildNode(MdElement.ListUnordered);
+            IMdNode listNode = currentNode.AddChildNode(ListType);
             for (int i = 0; i < matchCount; i++) {
                 GroupCollection groups = matchArray[i].Groups;
 
-                IMdNode listItemNode = ulNode.AddChildNode(MdElement.ListItem);
-
-                if (groups[LBodyId].TryGetValueSpan(out ReadOnlySpan<char> listBody) && !listBody.IsEmpty) {
-                    string normalizedBody = NormalizationHelper.NormalizeIndentation(ref listBody);
+                IMdNode listItemNode = listNode.AddChildNode(MdElement.ListItem);
+            
+                if (groups[LBodyId].TryGetValueSpan(out ReadOnlySpan<char> itemBody) && !itemBody.IsEmpty) {
+                    string normalizedBody = NormalizationHelper.NormalizeIndentation(ref itemBody);
                     parser.AddMultiLineMatchesToStack(normalizedBody, listItemNode, origin | ParserOrigin.PreserveHtml);
                 }
 
@@ -51,10 +54,18 @@ public class ListUnorderedSectionParser : ISectionHandler {
                 }
             }
         }
-
         finally {
             ArrayPool<Match>.Shared.Return(matchArray);
         }
-        
     }
+}
+
+[InjectableSingleton<ISectionHandler>("listOrdered")]
+public class ListOrderedSectionParserBase : ListSectionParserBase {
+    protected override MdElement ListType => MdElement.ListOrdered;
+}
+
+[InjectableSingleton<ISectionHandler>("listUnordered")]
+public class ListUnorderedSectionParser : ListSectionParserBase {
+    protected override MdElement ListType => MdElement.ListUnordered;
 }
