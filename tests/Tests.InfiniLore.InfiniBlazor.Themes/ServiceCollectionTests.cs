f4d91ef286d9fd0db7f2a7b04fe48a72481811ba
Anna Sas <anna.sas.dev@gmail.com>
2025-04-29 16:54:58 +0200

    Feat: Update tests and dependencies for theme registration
    Updated `CodeOfChaos.Testing.TUnit` to version 0.11.2 across projects, renamed namespaces for consistency, and added a test for theme registration in `ServiceCollectionTests`.

--- tests/Tests.InfiniLore.InfiniBlazor.Themes/ServiceCollectionTests.cs
+++ tests/Tests.InfiniLore.InfiniBlazor.Themes/ServiceCollectionTests.cs
@@ -4,10 +4,10 @@
 using CodeOfChaos.Testing.TUnit;
 using InfiniLore.InfiniBlazor.Themes;
 using InfiniLore.InfiniBlazor.Themes.Config;
+using InfiniLore.InfiniBlazor.Themes.Library;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Tests.InfiniLore.InfiniBlazor.Themes;
-
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
@@ -22,5 +22,17 @@ public class ServiceCollectionTests {
 
         // Assert
         await Assert.That(services).ContainsServiceType<IThemeSelector>();
+    }
+    
+    [Test]
+    public async Task ShouldAddServices_RegisterTheme() {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddInfiniBlazor(config => config.AddThemes(cfg => cfg.RegisterTheme<AnnaSasDevTheme>("anna")));
+
+        // Assert
+        await Assert.That(services).ContainsKeyedServiceImplementation<IInfiniLoreTheme, AnnaSasDevTheme>("anna");
     }
 }
