48dd84d057b2577d12dc8ba70c09d7c12703afdb
Anna Sas <anna.sas.dev@gmail.com>
2025-04-29 16:05:13 +0200

    Feat: Add service collection tests for Markdown and Themes

---
+++ tests/Tests.InfiniLore.InfiniBlazor.Markdown/ServiceCollectionTests.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Testing.TUnit;
+using InfiniLore.InfiniBlazor.Markdown;
+using InfiniLore.InfiniBlazor.Markdown.Config;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests.InfiniLore.InfiniBlazor.Markdown;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class ServiceCollectionTests {
+    [Test]
+    public async Task ShouldAddServices_AddThemes() {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddInfiniBlazor(config => config.AddMarkdown());
+
+        // Assert
+        await Assert.That(services).ContainsServiceImplementation(typeof(IMarkdownParser<,>), typeof(MarkdownParser<,>));
+    }
+}
---
+++ tests/Tests.InfiniLore.InfiniBlazor.Themes/ServiceCollectionTests.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Testing.TUnit;
+using InfiniLore.InfiniBlazor.Themes;
+using InfiniLore.InfiniBlazor.Themes.Config;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests.InfiniLore.InfiniBlazor.Themes;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class ServiceCollectionTests {
+    [Test]
+    public async Task ShouldAddServices_AddThemes() {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddInfiniBlazor(config => config.AddThemes());
+
+        // Assert
+        await Assert.That(services).ContainsServiceType<IThemeSelector>();
+    }
+}
