55c8dfa4be2f101360f7718a3b1ccdef6ff4bcf3
Anna Sas <anna.sas.dev@gmail.com>
2025-04-29 16:02:53 +0200

    Feat: Add Themes test project and enhance test setup

---
+++ tests/Tests.InfiniLore.InfiniBlazor/ServiceCollectionTests.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Testing.TUnit;
+using InfiniLore.InfiniBlazor.JsRuntime;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests.InfiniLore.InfiniBlazor;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class ServiceCollectionTests {
+    [Test]
+    public async Task ShouldAddServices() {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddInfiniBlazor();
+
+        // Assert
+        await Assert.That(services).ContainsServiceType<IJsRuntimeHelper>();
+    }
+}
