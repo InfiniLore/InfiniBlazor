ca4f893903ffd26d500c25fae7af4d21e0593170
Anna Sas <anna.sas.dev@gmail.com>
2025-04-30 11:08:33 +0200

    Chore: Improve readability of MarkdownParserFactory code

--- src/InfiniLore.InfiniBlazor.Markdown/Services/MarkdownParser/MarkdownParserFactory.cs
+++ src/InfiniLore.InfiniBlazor.Markdown/Services/MarkdownParser/MarkdownParserFactory.cs
@@ -13,11 +13,14 @@ namespace InfiniLore.InfiniBlazor.Markdown;
 public static class MarkdownParserFactory {
     public static IMarkdownParser<TInput, TOutput> CreateParser<TInput, TOutput>(IServiceProvider provider) => CreateKeyedParser<TInput, TOutput>(provider, null);
     public static IMarkdownParser<TInput, TOutput> CreateKeyedParser<TInput, TOutput>(IServiceProvider provider, object? key) {
-        ImmutableArray<IMarkdownInputProcessor<TInput>> inputProcessors = provider.GetRequiredKeyedService<IEnumerable<IMarkdownInputProcessor<TInput>>>(key)
+        ImmutableArray<IMarkdownInputProcessor<TInput>> inputProcessors = provider
+            .GetRequiredKeyedService<IEnumerable<IMarkdownInputProcessor<TInput>>>(key)
             .ToImmutableArray();
-        ImmutableArray<IMarkdownPostProcessor<TInput>> postProcessors = provider.GetRequiredKeyedService<IEnumerable<IMarkdownPostProcessor<TInput>>>(key)
+        ImmutableArray<IMarkdownPostProcessor<TInput>> postProcessors = provider
+            .GetRequiredKeyedService<IEnumerable<IMarkdownPostProcessor<TInput>>>(key)
             .ToImmutableArray();
-        ImmutableArray<IMarkdownOutputProcessor<TOutput>> outputProcessors = provider.GetRequiredKeyedService<IEnumerable<IMarkdownOutputProcessor<TOutput>>>(key)
+        ImmutableArray<IMarkdownOutputProcessor<TOutput>> outputProcessors = provider
+            .GetRequiredKeyedService<IEnumerable<IMarkdownOutputProcessor<TOutput>>>(key)
             .ToImmutableArray();
         
         var logger = provider.GetRequiredService<ILogger<MarkdownParser<TInput, TOutput>>>();
