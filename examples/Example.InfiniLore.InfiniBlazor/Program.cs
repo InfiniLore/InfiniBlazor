24e3054ff396531e9eddba3e31e44b84bc4b380a
Anna Sas <anna.sas.dev@gmail.com>
2025-05-01 17:58:23 +0200

    Feat: Add new CSS utilities and improve SideNavMenu interactions

--- examples/Example.InfiniLore.InfiniBlazor/Program.cs
+++ examples/Example.InfiniLore.InfiniBlazor/Program.cs
@@ -13,6 +13,8 @@ namespace Example;
 public class Program {
     public static void Main(string[] args) {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddLogging();
 
         builder.Services.AddInfiniBlazor(config => {
             config.AddMarkdownLogic();
