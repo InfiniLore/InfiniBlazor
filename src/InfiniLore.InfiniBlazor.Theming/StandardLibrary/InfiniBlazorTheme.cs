04e0c9f6de07d62ee758d9b961fb81392b7f0ab7
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-05-09 14:28:47 +0200

    Feat: Add gradient support to SidebarNav background

--- src/InfiniLore.InfiniBlazor.Theming/StandardLibrary/InfiniBlazorTheme.cs
+++ src/InfiniLore.InfiniBlazor.Theming/StandardLibrary/InfiniBlazorTheme.cs
@@ -57,9 +57,9 @@ public partial record InfiniBlazorTheme : ITheme {
     #endregion
     
     #region CascadedValues
-    [CssData, InterpretAsVar(nameof(ColorBase60))] public partial string SidebarNavBorderColor { get; }
+    [CssData, InterpretAsVar(nameof(ColorBase60))] public partial string SidebarNavBorderColor { get; init; }
     [CssData, InterpretAsVar(nameof(ColorBase90))] public partial string SidebarNavBackground { get; init; }
-    [CssData, InterpretAsVar(nameof(ColorBase70))] public partial string SidebarNavBackgroundHover { get; }
-    [CssData, InterpretAsVar(nameof(ColorBase00))] public partial string TextColor { get; }
+    [CssData, InterpretAsVar(nameof(ColorBase70))] public partial string SidebarNavBackgroundHover { get; init; }
+    [CssData, InterpretAsVar(nameof(ColorBase00))] public partial string TextColor { get; init; }
     #endregion
 }
