a743126e45c264035f9ebdb7a8f030726dd1e272
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-05-23 19:26:26 +0200

    Chore: Remove unused TypeScript contracts and tidy code format

--- src/InfiniLore.InfiniBlazor/TsLib/Contracts/Window.ts
+++ src/InfiniLore.InfiniBlazor/TsLib/Contracts/Window.ts
@@ -1,27 +1,23 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
-
+import {IInputElement} from "./IInputElement";
+import {CSharpTuple} from "./CSharpTuple";
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-export type IInputElement = HTMLInputElement | HTMLTextAreaElement
-export type CSharpTuple<T1, T2> = { "Item1": T1, "Item2": T2 };
-
-export type KeyCondition = (event: KeyboardEvent, key: string) => boolean;
-
 declare global {
     // noinspection JSUnusedGlobalSymbols
     interface Window {
         getInputSelectionStart: (element: IInputElement) => number;
         getInputSelectionEnd: (element: IInputElement) => number;
         getInputSelection: (element: IInputElement) => CSharpTuple<number, number>;
-        
+
         setInputSelectionRange: (element: IInputElement, start: number, end: number) => void;
 
         addPreventDefaultListener: () => void;
         removePreventDefaultListener: () => void;
-        
+
         setTextContent: (element: HTMLElement, text: string) => void;
         getTextContent: (element: HTMLElement) => string;
 
