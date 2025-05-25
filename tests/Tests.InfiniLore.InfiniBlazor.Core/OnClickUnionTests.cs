791f50ce494724aa09bf92e00f5647272e7fb634
Anna Sas <anna.sas.dev@gmail.com>
2025-05-25 15:01:15 +0200

    Feat: Add Arrange-Act-Assert pattern to OnClickUnionTests

--- tests/Tests.InfiniLore.InfiniBlazor.Core/OnClickUnionTests.cs
+++ tests/Tests.InfiniLore.InfiniBlazor.Core/OnClickUnionTests.cs
@@ -4,19 +4,22 @@
 using Infinilore.InfiniBlazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+using TUnit.Assertions.AssertConditions.Throws;
 
 namespace Tests.InfiniLore.InfiniBlazor.Core;
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
+[SuppressMessage("ReSharper", "ConvertToLocalFunction")]
 public class OnClickUnionTests {
     private static byte GetState(OnClickUnion union) {
         PropertyInfo? stateField = typeof(OnClickUnion).GetProperty("State", BindingFlags.NonPublic | BindingFlags.Instance);
         byte actualState = (byte)stateField!.GetValue(union)!;
         return actualState;
     }
-    
+
     // -----------------------------------------------------------------------------------------------------------------
     // Tests
     // -----------------------------------------------------------------------------------------------------------------
@@ -34,44 +37,49 @@ public class OnClickUnionTests {
 
     [Test]
     public async Task EventCallback_ShouldInvoke() {
+        // Arrange
         bool called = false;
         var args = new MouseEventArgs();
-
         EventCallback<MouseEventArgs> callback = EventCallback.Factory.Create<MouseEventArgs>(this, _ => called = true);
         OnClickUnion union = callback;
 
+        // Act
         await union.InvokeAsync(args);
+
+        // Assert
         await Assert.That(called).IsTrue();
     }
 
     [Test]
     public async Task Action_ShouldInvoke() {
+        // Arrange
         bool called = false;
-        OnClickUnion union = (Action)Action;
-
+        OnClickUnion union = new Action(() => called = true);
+        
+        // Act
         await union.InvokeAsync(new MouseEventArgs());
-        await Assert.That(called).IsTrue();
-        return;
 
-        void Action() => called = true;
+        // Assert
+        await Assert.That(called).IsTrue();
     }
 
     [Test]
     public async Task ActionWithArgs_ShouldInvoke() {
+        // Arrange
         MouseEventArgs? receivedArgs = null;
         var args = new MouseEventArgs();
-
-        OnClickUnion union = (Action<MouseEventArgs>)Action;
-
+        OnClickUnion union = new Action<MouseEventArgs>(e => receivedArgs = e);
+        
+        // Act
         await union.InvokeAsync(args);
-        await Assert.That(receivedArgs).IsEqualTo(args);
-        return;
 
-        void Action(MouseEventArgs e) => receivedArgs = e;
+        // Assert
+        await Assert.That(receivedArgs).IsEqualTo(args);
     }
 
     [Test]
     public async Task AsyncFunc_ShouldInvoke() {
+        // Arrange
         bool called = false;
         Func<Task> action = () => {
             called = true;
@@ -79,30 +87,39 @@ public class OnClickUnionTests {
         };
         OnClickUnion union = action;
 
+        // Act
         await union.InvokeAsync(new MouseEventArgs());
+
+        // Assert
         await Assert.That(called).IsTrue();
     }
 
     [Test]
     public async Task AsyncFuncWithArgs_ShouldInvoke() {
+        // Arrange
         MouseEventArgs? receivedArgs = null;
         var args = new MouseEventArgs();
-
         Func<MouseEventArgs, Task> action = e => {
             receivedArgs = e;
             return Task.CompletedTask;
         };
         OnClickUnion union = action;
 
+        // Act
         await union.InvokeAsync(args);
+
+        // Assert
         await Assert.That(receivedArgs).IsEqualTo(args);
     }
 
     [Test]
     public async Task NullDelegates_ShouldNotThrow() {
+        // Arrange
         OnClickUnion union = new();
-        await union.InvokeAsync(new MouseEventArgs());
-        // Test passes if no exception is thrown
+        var args = new MouseEventArgs();
+
+        // Act & Assert
+        await Assert.That(async () => await union.InvokeAsync(args)).ThrowsNothing();
     }
 
     [Test]
@@ -112,6 +129,7 @@ public class OnClickUnionTests {
     [Arguments(4)]// Func<Task>
     [Arguments(5)]// Func<MouseEventArgs, Task>
     public async Task State_ShouldBeCorrect(byte expectedState) {
+        // Arrange
         OnClickUnion union = expectedState switch {
             1 => EventCallback.Factory.Create<MouseEventArgs>(this, _ => {}),
             2 => new Action(() => {}),
@@ -121,10 +139,10 @@ public class OnClickUnionTests {
             _ => throw new ArgumentException("Invalid state")
         };
 
-        // Use reflection to access private State field
-        PropertyInfo? stateField = typeof(OnClickUnion).GetProperty("State", BindingFlags.NonPublic | BindingFlags.Instance);
-        byte actualState = (byte)stateField!.GetValue(union)!;
+        // Act
+        byte actualState = GetState(union);
 
+        // Assert
         await Assert.That(actualState).IsEqualTo(expectedState);
     }
 }
