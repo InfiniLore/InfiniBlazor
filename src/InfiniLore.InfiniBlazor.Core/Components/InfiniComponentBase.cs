56f8f9b799c9aace0d5fc62403262a738cd4aab3
Anna Sas <anna.sas.dev@gmail.com>
2025-06-06 17:59:08 +0200

    Feat: Add Debugger integration and toggle functionality

---
+++ src/InfiniLore.InfiniBlazor.Core/Components/InfiniComponentBase.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.Debugger;
+using Microsoft.AspNetCore.Components;
+
+namespace Infinilore.InfiniBlazor.Components;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public abstract class InfiniComponentBase : ComponentBase, IAsyncDisposable {
+    [Inject] protected IDebuggerProvider Debugger { get; set; } = null!;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    protected override void OnInitialized() {
+        base.OnInitialized();
+        Debugger.OnChange += StateHasChanged;
+    }
+
+    public ValueTask DisposeAsync() {
+        Debugger.OnChange -= StateHasChanged;
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
---
+++ src/InfiniLore.InfiniBlazor.Core/Components/InfiniDebugger/DebuggerProvider.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Extensions.DependencyInjection;
+using InfiniLore.InfiniBlazor.Debugger;
+
+namespace InfiniLore.InfiniBlazor.Components;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[InjectableScoped<IDebuggerProvider>]
+public class DebuggerProvider : IDebuggerProvider {
+    private DebuggerState State { get; set; } = DebuggerState.Disabled;
+    
+    public event Action? OnChange;
+    
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public DebuggerState GetState() => State;
+    public Task SetStateAsync(DebuggerState state) {
+        State = state;
+        OnChange?.Invoke();
+        return Task.CompletedTask;
+    }
+}
