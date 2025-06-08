bb995fd53a374886d03289ae047508631427b1d5
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-06-08 23:52:26 +0200

    Change: Dependency rework

--- src/InfiniLore.InfiniBlazor/Config/ServiceCollectionExtensions.cs
+++ src/InfiniLore.InfiniBlazor/Config/ServiceCollectionExtensions.cs
@@ -3,7 +3,6 @@
 // ---------------------------------------------------------------------------------------------------------------------
 using InfiniLore.InfiniBlazor;
 using InfiniLore.InfiniBlazor.Config;
-using InfiniLore.InfiniBlazor.Core;
 using InfiniLore.InfiniBlazor.Theming.Config;
 using InfiniLore.InfiniBlazor.Toasting.Config;
 
@@ -16,7 +15,6 @@ public static class ServiceCollectionExtensions {
     public static IServiceCollection AddInfiniBlazor(this IServiceCollection services, Action<IInfiniBlazorConfig>? configure = null) {
         var config = new InfiniBlazorConfig(services);
         services.RegisterServicesFromInfiniLoreInfiniBlazor();
-        services.RegisterServicesFromInfiniLoreInfiniBlazorCore();
         services.AddLucideIcons();
         config.AddThemingLogic(); // Adds the basics for Theming
         config.AddToastingLogic(); // Adds the basics for Toasting
