d42dcef9ceb59f73a3e6dd39cbe61c46e7752b43
Anna Sas <anna.sas.dev@gmail.com>
2025-06-17 17:13:25 +0200

    Refactor: Reorganize SectionInput and SectionCodeBlock order in ExampleHtmlPage, clean up redundant CSS classes, and add new blur utility variables and button backdrop filter styles.

--- examples/Example.InfiniBlazor.Shared/Components/Pages/ExampleHtmlPage/SectionCodeBlock.razor.cs
+++ examples/Example.InfiniBlazor.Shared/Components/Pages/ExampleHtmlPage/SectionCodeBlock.razor.cs
@@ -7,7 +7,7 @@ namespace Example.InfiniBlazor.Shared.Components.Pages.ExampleHtmlPage;
 // -----------------------------------------------------------------------------------------------------------------
 public partial class SectionCodeBlock {
     private string CSharpCode { get; set; } = """
-        var example = "Hello, World!";
+        var example = "Hello, World!";var example = "Hello, World!";var example = "Hello, World!";var example = "Hello, World!";var example = "Hello, World!";var example = "Hello, World!";var example = "Hello, World!";
         Console.WriteLine(example);
         
         if (something){
