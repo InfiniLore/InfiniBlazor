c48ff11b14629db075ee795378d2bfe57812e198
Anna Sas <anna.sas.dev@gmail.com>
2025-06-17 14:22:24 +0200

    Refactor: Consolidate InfiniTextInput and InfiniToggleInput into modular InfiniInput structure with shared base and layout components.

---
+++ src/InfiniLore.InfiniBlazor/Components/InfiniInput/InfiniInputBase.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Infinilore.InfiniBlazor.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace InfiniLore.InfiniBlazor.Components;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public abstract class InfiniInputBase : InfiniComponentBase {
+    [Parameter] public string Id { get; set; } = Guid.NewGuid().ToString("N");
+    
+    [Parameter] public string? Label { get; set; }
+    [Parameter] public LabelLocation LabelLocation { get; set; } = LabelLocation.Left;
+    
+    [Parameter] public string? ValidationMessage { get; set; }
+    
+    [Parameter] public bool Disabled { get; set; }
+    protected override bool IsDisabled => Disabled || base.IsDisabled;
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    protected override void OnParametersSet() {
+        base.OnParametersSet();
+        CascadedDisabled = Disabled;
+    }
+}
