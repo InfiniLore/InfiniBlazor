06c6aecab18ec4321aca92e9871fd4627801625c
Anna Sas <anna.sas.dev@gmail.com>
2025-06-17 16:01:38 +0200

    Refactor: Standardize formatting across components, remove unused imports, and improve whitespace consistency.

--- src/InfiniLore.InfiniBlazor/Components/InfiniInput/InfiniInputBase.cs
+++ src/InfiniLore.InfiniBlazor/Components/InfiniInput/InfiniInputBase.cs
@@ -5,18 +5,17 @@ using Infinilore.InfiniBlazor.Components;
 using Microsoft.AspNetCore.Components;
 
 namespace InfiniLore.InfiniBlazor.Components;
-
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 public abstract class InfiniInputBase : InfiniComponentBase {
     [Parameter] public string Id { get; set; } = Guid.NewGuid().ToString("N");
-    
+
     [Parameter] public string? Label { get; set; }
     [Parameter] public LabelLocation LabelLocation { get; set; } = LabelLocation.Left;
-    
+
     [Parameter] public string? ValidationMessage { get; set; }
-    
+
     [Parameter] public bool Disabled { get; set; }
     protected override bool IsDisabled => Disabled || base.IsDisabled;
 
