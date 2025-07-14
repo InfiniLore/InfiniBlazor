748686b82f0bd650a792cb173eb745cdcc0ac12b
Anna Sas <anna.sas.dev@gmail.com>
2025-07-14 16:34:25 +0200

    Feat: Add `ToFlexCol` extension to `Direction` enum and alias values for better flexibility in layout handling.

--- src/InfiniLore.InfiniBlazor.Contracts/Direction.cs
+++ src/InfiniLore.InfiniBlazor.Contracts/Direction.cs
@@ -7,11 +7,15 @@ namespace InfiniLore.InfiniBlazor;
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 public enum Direction {
-    LeftToRight,
-    RightToLeft
+    LeftToRight = 0,
+    Ltr = LeftToRight,
+    
+    RightToLeft = 1,
+    Rtl = RightToLeft,
 }
 
 
 public static class DirectionExtensions {
     public static string ToFlexRow(this Direction direction) => direction is Direction.LeftToRight ? "flex-row" : "flex-row-reverse";
+    public static string ToFlexCol(this Direction direction) => direction is Direction.LeftToRight ? "flex-col" : "flex-col-reverse";
 }
