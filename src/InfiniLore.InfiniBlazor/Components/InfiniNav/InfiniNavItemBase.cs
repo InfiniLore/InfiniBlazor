e66e8881a346e445744817993e1d8f0015207d43
Anna Sas <anna.sas.dev@gmail.com>
2025-07-16 15:23:47 +0200

    Refactor: Remove unused `InfiniSideNav` component, migrate to `InfiniNavBar` with `IconName` property overhaul, streamline `InfiniNav` components, enhance icon handling with `LucideIcon`, and replace `TopNav`/`SideNav` with `NavBarBottom`/`NavBarLeft` in layouts.
    **Twitch Redeems**
    - brought to you by `mybackhurtss_h` with : "feat: add celeste easteregg"
    - brought to you by `Stoney_Eagle` with : "Remove `Program.cs` before doing anything else."

--- src/InfiniLore.InfiniBlazor/Components/InfiniNav/InfiniNavItemBase.cs
+++ src/InfiniLore.InfiniBlazor/Components/InfiniNav/InfiniNavItemBase.cs
@@ -10,6 +10,8 @@ namespace InfiniLore.InfiniBlazor.Components;
 // ---------------------------------------------------------------------------------------------------------------------
 public class InfiniNavItemBase : InfiniComponentBase {
     [CascadingParameter] public bool NavBarCollapsed { get; set; }
+    [CascadingParameter] public Size NavBarSize { get; set; }
+    
     [Parameter] public bool HiddenOnCollapsed { get; set; }
     protected bool Hidden => NavBarCollapsed && HiddenOnCollapsed;
 }
