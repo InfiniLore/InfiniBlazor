3ad0304383bfbf0589ae18e3e98e04132b7f4df1
Anna Sas <anna.sas.dev@gmail.com>
2025-07-18 16:07:45 +0200

    Refactor: Centralize z-index values in `ZIndex` class, update component z-index usage for consistency, and extend CSS utility classes.

---
+++ src/InfiniLore.InfiniBlazor/ZIndex.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+namespace InfiniLore.InfiniBlazor;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class ZIndex {
+    public const string BlazorError = "z-1000";
+    public const string ToastMessageContainer = "z-900";
+    public const string DebugRenderInfo = "z-50";
+    public const string NavSubMenuItem = "z-30";
+    public const string Tooltip = "z-20";
+    public const string TableHeader = "z-15";
+    public const string CodeBlockCopyButton = "z-10";
+}
