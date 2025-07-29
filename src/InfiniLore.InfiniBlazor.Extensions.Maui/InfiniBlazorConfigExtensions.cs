464f6d826e2d50b07ea310b3ddb2015ac6f0db9e
Anna Sas <anna.sas.dev@gmail.com>
2025-07-29 19:48:00 +0200

    Refactor: Update project description for MAUI focus and remove unused `RootNamespace` property. Enhance comment clarity in `SetRenderModeForMauiBlazorHybrid`.

--- src/InfiniLore.InfiniBlazor.Extensions.Maui/InfiniBlazorConfigExtensions.cs
+++ src/InfiniLore.InfiniBlazor.Extensions.Maui/InfiniBlazorConfigExtensions.cs
@@ -10,7 +10,7 @@ namespace InfiniLore.InfiniBlazor.Config;
 public static class InfiniBlazorConfigExtensions {
     
     public static InfiniBlazorConfig SetRenderModeForMauiBlazorHybrid(this InfiniBlazorConfig config) {
-        // For MAUI this has to be set to null;
+        // For MAUI this has to be set to null so it can overtake it as WebView
         RenderModeProvider.InfiniRenderMode = null!;
         return config;
     }
