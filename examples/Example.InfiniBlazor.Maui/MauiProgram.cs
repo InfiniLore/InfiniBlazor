e7e6650fa018e9085bd168496b4ed61de4c36eff
Anna Sas <anna.sas.dev@gmail.com>
2025-07-29 19:16:14 +0200

    Add initial example project for `Example.InfiniBlazor.Maui` integrating platform-specific resources and configurations.

---
+++ examples/Example.InfiniBlazor.Maui/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Example.InfiniBlazor;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class MauiProgram {
+    public static MauiApp CreateMauiApp() {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .AsAnnaSasDevServerConsole(
+                24,
+                configure: asyncConsoleConfig => asyncConsoleConfig.ApplyThemeToRedirectedOutput = true
+            )
+            .CreateLogger();
+
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+
+        // WTF why do I need to set this to null? https://learn.microsoft.com/en-us/aspnet/core/blazor/hybrid/tutorials/maui-blazor-web-app?view=aspnetcore-9.0
+        builder.Services.AddInfiniBlazor(config => config.SetRenderMode(null!));
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddSerilog();
+
+        builder.Services.AddBlazorWebViewDeveloperTools();
+
+        return builder.Build();
+    }
+}
