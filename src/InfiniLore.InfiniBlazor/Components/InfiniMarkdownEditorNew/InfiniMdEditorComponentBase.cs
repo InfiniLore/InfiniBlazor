5b6279e80fabdc17a17dae0b64bb4abbf22d5d34
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-08-15 10:44:04 +0200

    Feat: Implement new Markdown Editor component with updated architecture and example page

---
+++ src/InfiniLore.InfiniBlazor/Components/InfiniMarkdownEditorNew/InfiniMdEditorComponentBase.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.Markdown;
+using Microsoft.AspNetCore.Components;
+
+namespace InfiniLore.InfiniBlazor.Components;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public abstract class InfiniMdEditorComponentBase : InfiniComponentBase {
+    [CascadingParameter] protected MdEditorContext EditorContext { get; set; } = MdEditorContext.Empty;
+    
+    
+    // Some helpful quick handles for the EditorContext
+    protected bool IsLocked => EditorContext.IsLocked;
+    protected ITextSource TextSource => EditorContext.TextSource;
+}
---
+++ src/InfiniLore.InfiniBlazor/Components/InfiniMarkdownEditorNew/MdEditorContext.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.Markdown;
+using InfiniLore.InfiniBlazor.TextEditor;
+using Microsoft.AspNetCore.Components;
+
+namespace InfiniLore.InfiniBlazor.Components;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class MdEditorContext {
+    public bool IsLocked { get; set; }
+    public ITextSource TextSource { get; set; } = new TextSource();
+    public ElementReference ContentRef { get; set; }
+
+    public static MdEditorContext Empty => new();
+}
