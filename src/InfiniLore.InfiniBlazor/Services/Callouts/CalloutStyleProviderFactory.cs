c7309a47e980f8c0c0c31c2fb503b33ba01892a9
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-08-18 13:32:28 +0200

    Refactor: Update callout text color styles and add new CSS variables for improved consistency in theming

--- src/InfiniLore.InfiniBlazor/Services/Callouts/CalloutStyleProviderFactory.cs
+++ src/InfiniLore.InfiniBlazor/Services/Callouts/CalloutStyleProviderFactory.cs
@@ -14,29 +14,29 @@ public static class CalloutStyleProviderFactory {
             ["note"] = (
                 LucideNames.Quote,
                 "border-neutral-400 bg-zinc-900 text-neutral-300",
-                "border-neutral-600 text-neutral-500"
+                "border-neutral-600 text-neutral-400"
             ),
             ["warning"] = (
                 LucideNames.TriangleAlert, 
                 "border-orange-500 bg-amber-950 text-orange-300",
-                "border-orange-700 text-orange-500"
+                "border-orange-700 text-orange-400"
             ),
             ["tip"] = (LucideNames.Lightbulb, 
                 "border-violet-500 bg-violet-950 text-violet-300", 
-                "border-violet-700 text-violet-500" 
+                "border-violet-700 text-violet-400" 
             ),
             ["danger"] = (LucideNames.Flame, 
                 "border-red-500 bg-red-950 text-red-300", 
-                "border-red-700 text-red-500" 
+                "border-red-700 text-red-400" 
             ),
             ["info"] = (LucideNames.Info, 
                 "border-blue-500 bg-blue-950 text-blue-300", 
-                "border-blue-700 text-blue-500" 
+                "border-blue-700 text-blue-400" 
             ),
             ["success"] = (
                 LucideNames.CircleCheck, 
                 "border-green-500 bg-green-950 text-green-300",
-                "border-green-700 text-green-500"
+                "border-green-700 text-green-400"
             )
         };
 
