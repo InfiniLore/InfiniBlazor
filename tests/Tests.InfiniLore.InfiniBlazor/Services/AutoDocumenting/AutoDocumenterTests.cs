7b306a51fd1d6b411962a29fd66cf98fcc6f656d
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-08-28 13:05:02 +0200

    Feature: Add unit tests for AutoDocumenter service and integrate shared test project for auto-documentation

---
+++ tests/Tests.InfiniLore.InfiniBlazor/Services/AutoDocumenting/AutoDocumenterTests.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.AutoDocumenting;
+using Microsoft.AspNetCore.Components;
+using Tests.Shared.InfiniLore.InfiniBlazor.AutoDocumenting.DataSources;
+
+namespace Tests.InfiniLore.InfiniBlazor.Services.AutoDocumenting;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class AutoDocumenterTests {
+    private AutoDocumenter AutoDocumenter { get; } = new();
+    
+    // -----------------------------------------------------------------------------------------------------------------
+    // Test Data
+    // -----------------------------------------------------------------------------------------------------------------
+    public static IEnumerable<Func<(RenderFragment, string)>> GetRenderFragments() {
+        yield return () => (
+            builder => {
+                builder.OpenComponent(0, typeof(ParagraphTest));
+                builder.CloseComponent();
+            },
+            """
+            <ParagraphTest>
+                <p>Something</p>
+                <p>Else</p>
+            </ParagraphTest>
+            """
+        );
+    }
+    
+    // -----------------------------------------------------------------------------------------------------------------
+    // Test Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    [Test]
+    [MethodDataSource(nameof(GetRenderFragments))]
+    public async Task Render_ReturnsExpected(RenderFragment renderFragment, string expected) {
+        // Arrange
+        
+        // Act
+        string result = AutoDocumenter.ConvertToString(renderFragment);
+
+        // Assert
+        await Assert.That(result).IsEqualTo(expected);
+    } 
+}
