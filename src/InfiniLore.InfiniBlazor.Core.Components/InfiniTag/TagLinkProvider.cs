cc50fc2accdca87ebf96ab47e9134fb994fefbf2
Anna Sas <anna.sas.dev@gmail.com>
2025-09-09 17:43:56 +0200

    Feat : Foundation for first class citizen `InfiniTag` (#116)
    * Feat: Add `InfiniTag` component and `InfiniSectionTags`
    
    - Introduced `InfiniTag` reusable link component with customizable properties.
    - Added `SectionTags` to display and utilize `InfiniTag` in an example page.
    - Enhanced stylesheet (`InfiniBlazor.css`) with new utility classes for spacing, color variations, and accessibility improvements.
    - Updated `.DotSettings` to exclude new namespace in inspections.
    
    * Refactor: Simplify HTML structures and replace `<InfiniSection>` with native tags
    
    * Feat: Expand `InfiniTag` flexibility with new interfaces and content rendering updates
    
    * Feat: Enhance `InfiniTag` and Markdown integration with context-aware rendering
    
    * Feat: Refactor `InfiniTag` component with `HrefOverload` parameter and improve shared handling for `TagLinkContext`
    
    * Refactor: Introduce `InfiniHrefContentComponentBase` for shared href logic and refactor components to inherit it

---
+++ src/InfiniLore.InfiniBlazor.Core.Components/InfiniTag/TagLinkProvider.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Extensions.DependencyInjection;
+using InfiniLore.InfiniBlazor.Components.Tags;
+using System.Diagnostics.CodeAnalysis;
+
+namespace InfiniLore.InfiniBlazor.Components;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[InjectableSingleton<ITagLinkProvider>]
+public class TagLinkProvider : ITagLinkProvider {
+    
+    public bool TryGetLink(string tag, [NotNullWhen(true)] out string? link) {
+        link = null;
+        return false;
+    }
+}
