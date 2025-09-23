5acb6d2480c7725ab4def3739c2687e88c2565cb
Anna Sas <anna.sas.dev@gmail.com>
2025-09-23 13:17:52 +0200

    Feat: Add deserializer for `==highlight==` syntax in Markdown.parsers

---
+++ src/InfiniLore.InfiniBlazor.Core.Markdown/Parsers/MarkdownString/Deserializer/NodeDeserializers/HighlightSyntaxNodeDeserializer.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.Markdown.Syntax.Nodes;
+using System.Text;
+
+namespace InfiniLore.InfiniBlazor.Markdown.Parsers.MarkdownString.Deserializer.NodeDeserializers;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public sealed class HighlightSyntaxNodeDeserializer : MdStringMdSyntaxNodeDeserializerBase<HighlightMdSyntaxNode> {
+
+    protected override void Deserialize(HighlightMdSyntaxNode node, StringBuilder builder) {
+        builder.Append("==");
+        DeserializeChildren(node, builder);
+        builder.Append("==");
+    }
+}
