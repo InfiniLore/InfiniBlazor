38ca49ebaa65b73da10ae50eac62845759c1df86
Anna Sas <anna.sas.dev@gmail.com>
2025-09-23 13:13:34 +0200

    Feat: Add support for `==highlight==` syntax in Markdown parsers and serializers

---
+++ src/InfiniLore.InfiniBlazor.Core.Markdown/Parsers/MarkdownString/Serializer/NodeSerializers/HighlightSyntaxNodeSerializer.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.InfiniBlazor.Markdown.Parsers.MarkdownString.Serializer.RegexLib;
+using InfiniLore.InfiniBlazor.Markdown.Syntax.Nodes;
+using System.Text.RegularExpressions;
+
+namespace InfiniLore.InfiniBlazor.Markdown.Parsers.MarkdownString.Serializer.NodeSerializers;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class HighlightSyntaxNodeSerializer  {
+    private static readonly int HId = MdRegexLib.GetGroupId(MdRegexGroupNames.HighlightContent);
+    
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public static void Serialize(IMdSyntaxFragmentStack stack, IMdSyntaxNode parentNode, Match match) {
+        if (!match.Groups[HId].TryGetValue(out string? boldValue)) return;
+
+        HighlightMdSyntaxNode node = HighlightMdSyntaxNode.Pool.Get();
+        parentNode.AddChildNode(node);
+        stack.PushSingleLineMatchesToStack(boldValue, node);
+    }
+}
