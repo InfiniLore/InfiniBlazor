ae16b1196c6cc2dd31080e85d024d26dbbd269e8
Anna Sas <anna.sas.dev@gmail.com>
2025-09-24 16:00:28 +0200

    Refactor: Use scoped service provider in `HtmlMdSyntaxTreeParser` and remove redundant button property.

--- src/InfiniLore.InfiniBlazor.Core.Markdown/Parsers/Html/HtmlMdSyntaxTreeParser.cs
+++ src/InfiniLore.InfiniBlazor.Core.Markdown/Parsers/Html/HtmlMdSyntaxTreeParser.cs
@@ -6,6 +6,7 @@ using InfiniLore.InfiniBlazor.Markdown.Parsers.Blazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.Web.HtmlRendering;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace InfiniLore.InfiniBlazor.Markdown.Parsers.Html;
@@ -16,7 +17,8 @@ namespace InfiniLore.InfiniBlazor.Markdown.Parsers.Html;
 [InjectableSingleton<IHtmlMdSyntaxTreeParser>]
 public class HtmlMdSyntaxTreeParser(IServiceProvider provider, ILoggerFactory loggerFactory) : IHtmlMdSyntaxTreeParser {
     public async Task<string> DeserializeToStringAsync(IMdSyntaxTree tree, CancellationToken ct = default) {
-        await using var htmlRenderer = new HtmlRenderer(provider, loggerFactory);
+        await using AsyncServiceScope scope = provider.CreateAsyncScope();
+        await using var htmlRenderer = new HtmlRenderer(scope.ServiceProvider, loggerFactory);
         string output = await htmlRenderer.Dispatcher.InvokeAsync(async () => {
             await using var textWriter = new StringWriter();
             
