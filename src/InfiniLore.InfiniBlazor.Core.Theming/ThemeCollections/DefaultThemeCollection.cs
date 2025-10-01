f78e3b5f69920a24887a4fefc14830e1206b0720
Anna Sas <anna.sas.dev@gmail.com>
2025-10-01 17:38:05 +0200

    Refactor: Rename CSS properties and fields to align with `InfiniBlazor` naming conventions across theming.

--- src/InfiniLore.InfiniBlazor.Core.Theming/ThemeCollections/DefaultThemeCollection.cs
+++ src/InfiniLore.InfiniBlazor.Core.Theming/ThemeCollections/DefaultThemeCollection.cs
@@ -17,24 +17,24 @@ public class DefaultThemeCollection : ThemeCollection {
         [ThemeMode.DarkMode] = EmptyCssData.Instance,
         
         [ThemeMode.LightMode] = EmptyCssData.Instance with {
-            ColorBase00 = "var(--color-infini-base-100)",
-            ColorBase05 = "var(--color-infini-base-95)",
-            ColorBase10 = "var(--color-infini-base-90)",
-            ColorBase20 = "var(--color-infini-base-80)",
-            ColorBase30 = "var(--color-infini-base-70)",
-            ColorBase40 = "var(--color-infini-base-60)",
-            ColorBase50 = "var(--color-infini-base-50)",
-            ColorBase60 = "var(--color-infini-base-40)",
-            ColorBase70 = "var(--color-infini-base-30)",
-            ColorBase80 = "var(--color-infini-base-20)",
-            ColorBase90 = "var(--color-infini-base-10)",
-            ColorBase95 = "var(--color-infini-base-05)",
-            ColorBase100 = "var(--color-infini-base-00)",
+            ColorInfiniBase00 = "var(--color-infini-base-100)",
+            ColorInfiniBase05 = "var(--color-infini-base-95)",
+            ColorInfiniBase10 = "var(--color-infini-base-90)",
+            ColorInfiniBase20 = "var(--color-infini-base-80)",
+            ColorInfiniBase30 = "var(--color-infini-base-70)",
+            ColorInfiniBase40 = "var(--color-infini-base-60)",
+            ColorInfiniBase50 = "var(--color-infini-base-50)",
+            ColorInfiniBase60 = "var(--color-infini-base-40)",
+            ColorInfiniBase70 = "var(--color-infini-base-30)",
+            ColorInfiniBase80 = "var(--color-infini-base-20)",
+            ColorInfiniBase90 = "var(--color-infini-base-10)",
+            ColorInfiniBase95 = "var(--color-infini-base-05)",
+            ColorInfiniBase100 = "var(--color-infini-base-00)",
         
-            ButtonDefault = "var(--color-infini-base-10)",
-            ButtonPrimary = "var(--color-infini-base-80)",
+            ColorInfiniButtonDefault = "var(--color-infini-base-10)",
+            ColorInfiniButtonPrimary = "var(--color-infini-base-80)",
             
-            Section = "var(--color-infini-base-10)",
+            ColorInfiniSection = "var(--color-infini-base-10)",
         }
     };
 }
