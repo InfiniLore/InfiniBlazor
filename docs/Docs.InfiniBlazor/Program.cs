2f6216267caebd5113f708b6bfbd48e716b2db12
Anna Sas <anna.sas.dev@gmail.com>
2025-10-28 16:25:14 +0100

    cleanup

--- docs/Docs.InfiniBlazor/Program.cs
+++ docs/Docs.InfiniBlazor/Program.cs
@@ -1,7 +1,6 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
-using Docs.InfiniBlazor.Layout;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
