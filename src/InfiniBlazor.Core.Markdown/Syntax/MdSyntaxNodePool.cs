a5c61257e06ed6cd492f88cf7f967110a4a6ca49
Anna Sas <anna.sas.dev@gmail.com>
2026-01-04 12:08:44 +0100

    Refactor: Remove unused `System.Runtime.CompilerServices` import and redundant `Return(IMdSyntaxNode)` method in `MdSyntaxNodePool`

--- src/InfiniBlazor.Core.Markdown/Syntax/MdSyntaxNodePool.cs
+++ src/InfiniBlazor.Core.Markdown/Syntax/MdSyntaxNodePool.cs
@@ -3,7 +3,6 @@
 // ---------------------------------------------------------------------------------------------------------------------
 using InfiniBlazor.Pooling;
 using Microsoft.Extensions.ObjectPool;
-using System.Runtime.CompilerServices;
 
 namespace InfiniBlazor.Markdown.Syntax;
 
@@ -14,8 +13,10 @@ public class MdSyntaxNodePool<T> where T : class, IMdSyntaxNode, new() {
     public static MdSyntaxNodePool<T> Shared { get; } = new();
     
     private static ObjectPool<T> Pool { get; } = PoolingHelpers.CreateResettablePool<T>(PoolingHelpers.VisitorPerParserRetained);
-    
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
     public T Get() => Pool.Get();
     public void Return(T node) => Pool.Return(node);
-    public void Return(IMdSyntaxNode node) => Pool.Return(Unsafe.As<T>(node));
 }
