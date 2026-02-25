d46c531146ba78a4093dd76c6cd4b7381529e16f
Anna Sas <anna.sas.dev@gmail.com>
2026-02-25 13:43:13 +0100

    Refactor: Adjust `DefaultMultiLineRegexOptions` order for consistency and add null/whitespace input check in `TryGetMatch`.

--- src/InfiniBlazor.Core.Markdown/Parsers/Markdown/Serializer/BaseMdSyntaxNodeSerializer.cs
+++ src/InfiniBlazor.Core.Markdown/Parsers/Markdown/Serializer/BaseMdSyntaxNodeSerializer.cs
@@ -12,7 +12,7 @@ namespace InfiniBlazor.Markdown.Parsers.Markdown.Serializer;
 // ---------------------------------------------------------------------------------------------------------------------
 public abstract class BaseMdSyntaxNodeSerializer : IMdSyntaxNodeSerializer {
     protected const RegexOptions DefaultSingleLineRegexOptions = RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace;
-    protected const RegexOptions DefaultMultiLineRegexOptions = RegexOptions.Multiline | RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace;
+    protected const RegexOptions DefaultMultiLineRegexOptions = RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline;
     
     protected abstract Regex Syntax { get; }
     public virtual ReadOnlySpan<char> TriggerCharacters => default;
@@ -23,6 +23,8 @@ public abstract class BaseMdSyntaxNodeSerializer : IMdSyntaxNodeSerializer {
     public bool TryGetMatch(string input, [NotNullWhen(true)] out Match? match, int startPosition = 0) {
         match = null;
         if (startPosition >= input.Length) return false;
+        if (input.IsNullOrWhiteSpace()) return false;
+        
         match = Syntax.Match(input, startPosition);
         return match.Success;
     }
